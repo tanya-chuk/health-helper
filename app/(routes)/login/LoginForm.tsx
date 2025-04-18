07bde1edfb548768e88d5fa52b5243e79b4179fb
Tanya Osadchuk <tanya-chuk@yandex.ru>
2025-04-18 18:54:52 +0200

    Updates login interface

--- app/(routes)/login/LoginForm.tsx
+++ app/(routes)/login/LoginForm.tsx
@@ -15,40 +15,48 @@ export const LoginForm = () => {
   return (
     <form action={formAction}>
       <div>
-        <h1>Please log in to continue.</h1>
-        <div>
+        <div style={{ height: '36px', display: 'flex', gap: 15 }}>
+          <label htmlFor="email" style={{ width: '80px' }}>
+            Email
+          </label>
           <div>
-            <label htmlFor="email">Email</label>
-            <div>
-              <input
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
-              />
-            </div>
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <div>
-              <input
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                required
-                minLength={6}
-              />
-            </div>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Enter your email address"
+              required
+              style={{ width: '200px', borderBottom: '1px solid darkgrey' }}
+            />
           </div>
         </div>
-        <input type="hidden" name="redirectTo" value={callbackUrl} />
-        <Button aria-disabled={isPending} type="submit">
-          Log in
-        </Button>
-        {errorMessage && <p>{errorMessage}</p>}
+        <div style={{ height: '36px', display: 'flex', gap: 15 }}>
+          <label htmlFor="password" style={{ width: '80px' }}>
+            Password
+          </label>
+          <div>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Enter password"
+              required
+              minLength={6}
+              style={{ width: '200px', borderBottom: '1px solid darkgrey' }}
+            />
+          </div>
+        </div>
       </div>
+      <input type="hidden" name="redirectTo" value={callbackUrl} />
+      <Button
+        aria-disabled={isPending}
+        type="submit"
+        variant="outlined"
+        sx={{ marginTop: '15px', textTransform: 'none' }}
+      >
+        Войти
+      </Button>
+      {errorMessage && <p>{errorMessage}</p>}
     </form>
   );
 };
