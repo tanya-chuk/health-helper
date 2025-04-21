a3c329f8c8b3638d79ec8e893679e82dba8f1370
Tanya Osadchuk <tanya-chuk@yandex.ru>
2025-04-21 14:48:47 +0200

    Adds documentation for the utility function

--- app/utils/stringifyReaderFriendly.ts
+++ app/utils/stringifyReaderFriendly.ts
@@ -1,3 +1,8 @@
+/**
+ * Transforms array of strings into a single string of words separated by commas
+ * @param list - an array of string values
+ * @returns - a string consisting of all array values separated by commas
+ */
 export const stringifyReaderFriendly = (list: Array<string>) => {
   if (!list.length) {
     return '';
