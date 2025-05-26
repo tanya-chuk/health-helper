fc7344ae5d727080f32c8f2d9f343a3eb1d78f2b
Tanya Osadchuk <tanya-chuk@yandex.ru>
2025-05-26 17:14:49 +0200

    Hides login from authenticated user

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,17 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+import { getToken } from 'next-auth/jwt';
+import { auth } from '@/auth';
+import { NextResponse } from 'next/server';
 
-export default NextAuth(authConfig).auth;
+export default auth(async function middleware(req) {
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const isAuthenticated = Boolean(token);
+
+  if (req.nextUrl.pathname.startsWith('/login')) {
+    if (isAuthenticated) {
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+  }
+});
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
